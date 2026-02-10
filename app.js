6e412a35748b2a0cb5f52b25618f055e78bd71de
BOODA <abodydade40@gmail.com>
2026-02-10 16:52:54 +0200

    Initialize Express application with essential middleware and routing

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+
+const tourRouter = require('./modules/tourModules/tourRout');
+const userRouter = require('./modules/userModels/userRout');
+const AppError = require('./common/errHandling/appError');
+const errorController = require('./common/errHandling/errorController');
+
+const app = express();
+// 1) GLOBAL MIDDLEWARES
+
+// Set Security HTTP headers
+app.use(helmet());
+
+// Limit requests from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
+
+// Use morgan for logging
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+// Serving static files
+app.use(express.static(`${__dirname}/public`));
+
+// Test middleware
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+// 2) ROUTES
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+// Handling unhandled routes
+app.all('*', (req, res, next) => {
+  next(new AppError('Can not find this route', 400));
+});
+
+// Global error handling middleware
+app.use(errorController);
+
+module.exports = app;
