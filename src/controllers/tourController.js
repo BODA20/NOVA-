28f24617ca97540db8b734513b1d0a40595df6c8
BOODA <abodydade40@gmail.com>
2026-04-25 17:47:09 +0300

    feat: implement modular architecture with tour, review, and admin service layers and add Jest testing framework

---
+++ src/controllers/tourController.js
@@ -0,0 +1,113 @@
+const tourService = require('../services/tourService');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+exports.getAllTours = catchAsync(async (req, res, next) => {
+  const tours = await tourService.getAllTours(req.query);
+
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: { tours },
+  });
+});
+
+exports.getTour = catchAsync(async (req, res, next) => {
+  const tour = await tourService.getTour(req.params.id);
+
+  if (!tour) {
+    return next(new AppError('No tour found with that ID', 404));
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: { tour },
+  });
+});
+
+exports.createTour = catchAsync(async (req, res, next) => {
+  const newTour = await tourService.createTour(req.body);
+
+  res.status(201).json({
+    status: 'success',
+    data: { tour: newTour },
+  });
+});
+
+exports.updateTour = catchAsync(async (req, res, next) => {
+  const tour = await tourService.updateTour(req.params.id, req.body);
+
+  if (!tour) {
+    return next(new AppError('No tour found with that ID', 404));
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: { tour },
+  });
+});
+
+exports.deleteTour = catchAsync(async (req, res, next) => {
+  const tour = await tourService.deleteTour(req.params.id);
+
+  if (!tour) {
+    return next(new AppError('No tour found with that ID', 404));
+  }
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
+
+exports.getToursWithin = catchAsync(async (req, res, next) => {
+  const { distance, latlng, unit } = req.params;
+  const [lat, lng] = latlng.split(',');
+
+  if (!lat || !lng) {
+    return next(
+      new AppError(
+        'Please provide latitude and longitude in the format lat,lng.',
+        400,
+      ),
+    );
+  }
+
+  const tours = await tourService.getToursWithin({
+    distance,
+    lat: lat * 1,
+    lng: lng * 1,
+    unit,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: { tours },
+  });
+});
+
+exports.getDistances = catchAsync(async (req, res, next) => {
+  const { latlng, unit } = req.params;
+  const [lat, lng] = latlng.split(',');
+
+  if (!lat || !lng) {
+    return next(
+      new AppError(
+        'Please provide latitude and longitude in the format lat,lng.',
+        400,
+      ),
+    );
+  }
+
+  const distances = await tourService.getDistances({
+    lat: lat * 1,
+    lng: lng * 1,
+    unit,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: { distances },
+  });
+});
